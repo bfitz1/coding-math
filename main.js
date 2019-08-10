4c28cbb515ceae4e92159b7f1dc15211e5dee7e7
Blair Fitzhugh <bfitz1@umbc.edu>
2019-08-09 22:55:34 -0400

    Add vector class

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-window.onload = function() {
-    let canvas = document.getElementById('canvas');
-    let context = canvas.getContext('2d');
-    let width = canvas.width = window.innerWidth;
-    let height = canvas.height = window.innerHeight;
+let v1 = new Vector(10, 5);
 
-    for (let i = 0; i < 100; i += 1) {
-        context.beginPath();
-        context.moveTo(Math.random() * width, Math.random() * height);
-        context.lineTo(Math.random() * width, Math.random() * height);
-        context.stroke();
-    }
-}
+console.log("Basic attributes and methods on a vector")
+console.log(`x: ${v1.x}, y: ${v1.y}, angle: ${v1.getAngle()}, length: ${v1.getLength()}`);
+
+console.log("Setting attributes on a vector")
+console.log("angle -> Math.PI / 6");
+v1.setAngle(Math.PI / 6);
+console.log("length -> 100");
+v1.setLength(100);
+console.log(`x: ${v1.x}, y: ${v1.y}`);
+
+console.log("Vector addition");
+let w1 = new Vector(10, 5);
+let w2 = new Vector(3, 4);
+let w3 = w1.add(w2);
+console.log(`x: ${w1.x}, y: ${w1.y}`);
+
+console.log("Scalar vector multiplicatoin")
+let u1 = new Vector(10, 5);
+console.log(`length: ${u1.getLength()}`);
+let u2 = u1.mul(2);
+console.log(`length: ${u2.getLength()}`);
---
+++ vector.js
@@ -0,0 +1,40 @@
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Vector.prototype.setAngle = function(angle) {
+    let length = this.getLength();
+    this.x = Math.cos(angle) * length;
+    this.y = Math.sin(angle) * length;
+}
+
+Vector.prototype.getAngle = function() {
+    return Math.atan2(this.y, this.x);
+}
+
+Vector.prototype.setLength = function(length) {
+    let angle = this.getAngle();
+    this.x = Math.cos(angle) * length;
+    this.y = Math.sin(angle) * length;
+}
+
+Vector.prototype.getLength = function() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+}
+
+Vector.prototype.add = function(v2) {
+    return new Vector(this.x + v2.x, this.y + v2.y);
+}
+
+Vector.prototype.sub = function(v2) {
+    return new Vector(this.x - v2.x, this.y - v2.y);
+}
+
+Vector.prototype.mul = function(s) {
+    return new Vector(this.x * s, this.y * s);
+}
+
+Vector.prototype.div = function(s) {
+    return new Vector(this.x / s, this.y / s);
+}
