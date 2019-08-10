dcfa1ea4ba51fdb2ac65b9023b93fbb2036b5f68
Blair Fitzhugh <bfitz1@umbc.edu>
2019-08-10 12:19:02 -0400

    Another acceleration example

--- main.js
+++ main.js
@@ -3,20 +3,96 @@ window.onload = function() {
     let context = canvas.getContext('2d');
     let width = canvas.width = window.innerWidth;
     let height = canvas.height = window.innerHeight;
-    let p = Particle.fromParts(100, height, 10, -Math.PI / 2);
-    let accel = new Vector(0.1, 0.1);
+    let ship = Particle.fromParts(width / 2, height / 2, 0, 0);
+    let thrust = new Vector(0, 0);
+    let angle = 0;
+    let turningLeft = false;
+    let turningRight = false;
+    let thrusting = false;
 
     update();
+
+    document.body.addEventListener('keydown', function(event) {
+        switch(event.keyCode) {
+            case 38: // up
+                thrusting = true;
+                break;
+            case 37: // left
+                turningLeft = true;
+                break;
+            case 39: // right
+                turningRight = true;
+                break;
+            default:
+                break;
+        }
+    });
+
+    document.body.addEventListener('keyup', function(event) {
+        switch(event.keyCode) {
+            case 38: // up
+                thrusting = false;
+                break;
+            case 37: // left
+                turningLeft = false;
+                break;
+            case 39: // right
+                turningRight = false;
+                break;
+            default:
+                break;
+        }
+    });
 
     function update() {
         context.clearRect(0, 0, width, height);
 
-        p.accelerate(accel);
-        p.update();
+        if (turningLeft) {
+            angle -= 0.05;
+        }
+        if (turningRight) {
+            angle += 0.05;
+        }
+
+        thrust.setAngle(angle);
+        if (thrusting) {
+            thrust.setLength(0.1);
+        } else {
+            thrust.setLength(0);
+        }
+
+        ship.accelerate(thrust);
+        ship.update();
+
+        context.save();
+        context.translate(ship.position.x, ship.position.y);
+        context.rotate(angle);
 
         context.beginPath();
-        context.arc(p.position.x, p.position.y, 10, 0, Math.PI * 2, false);
-        context.fill();
+        context.moveTo(10, 0);
+        context.lineTo(-10, -7);
+        context.lineTo(-10, 7);
+        context.lineTo(10, 0);
+        if (thrusting) {
+            context.moveTo(-10, 0);
+            context.lineTo(-18, 0);
+        }
+        context.stroke();
+
+        context.restore();
+
+        if (ship.position.x > width) {
+            ship.position.x = 0;
+        }
+        if (ship.position.x < 0) {
+            ship.position.x = width;
+        }
+        if (ship.position.y > height) {
+            ship.position.y = 0;
+        }
+        if (ship.position.y < 0) {
+            ship.position.y = height;
+        }
 
         requestAnimationFrame(update);
     }
