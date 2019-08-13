7b2d7db5a4bb83da92c7b2d66f4672693ea2d8f6
Blair Fitzhugh <bfitz1@umbc.edu>
2019-08-13 17:44:44 -0400

    Add spring physics example

---
+++ spring1.js
@@ -0,0 +1,45 @@
+window.onload = function() {
+    let canvas = document.getElementById('canvas');
+    let context = canvas.getContext('2d');
+    let width = canvas.width = window.innerWidth;
+    let height = canvas.height = window.innerHeight;
+    let springPoint = new Vector(width / 2, height / 2);
+    let weight = Particle.fromParts(Math.random() * width, Math.random() * height, 50, Math.random() * Math.PI * 2);
+    let k = 0.1;
+
+    weight.radius = 20;
+    weight.friction = 0.9;
+
+    document.body.addEventListener('mousemove', function(event) {
+        springPoint.x = event.clientX;
+        springPoint.y = event.clientY;
+    });
+
+    update();
+
+    function update() {
+        context.clearRect(0, 0, width, height);
+
+        let distance = springPoint.sub(weight.position);
+        let springForce = distance.mul(k);
+
+        weight.velocity.addTo(springForce);
+
+        weight.update();
+
+        context.beginPath();
+        context.arc(weight.position.x, weight.position.y, weight.radius, 0, Math.PI * 2, false);
+        context.fill();
+
+        context.beginPath();
+        context.arc(springPoint.x, springPoint.y, 4, 0, Math.PI * 2, false);
+        context.fill();
+
+        context.beginPath();
+        context.moveTo(weight.position.x, weight.position.y);
+        context.lineTo(springPoint.x, springPoint.y);
+        context.stroke();
+
+        requestAnimationFrame(update);
+    }
+}
